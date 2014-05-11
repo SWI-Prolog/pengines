35b99c3e3dc46118e19bcff0985bcea3ca825cfe
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-05-11 15:51:34 +0200

    Removed app psglab

--- run.pl
+++ run.pl
@@ -1,16 +1,15 @@
 % This system may only distributed using the GNU General Public License
 % because the following components contain GPL-ed code:
-% 
+%
 %     /opt/local/lib/swipl-6.3.15/library/mime.pl
 %     GNU Readline library
-% 
+%
 % See http://www.swi-prolog.org/license.html for details on
 % SWI-Prolog licensing policies supporting both free and non-free
 % Software.
 
-:- op(1200, xfy, (--->)).
-
 :- use_module(library(pengines)).
+:- use_module(library(http/http_error)).
 :- use_module(server).
 :- use_module(storage).
 
@@ -20,9 +19,6 @@
 
 :- pengine_application(test).
 :- use_module(test:apps/scratchpad/examples/test_app).
-
-:- pengine_application(psglab).
-:- use_module(psglab:apps/psglab/parser).
 
 :- pengine_application(mothers).
 :- use_module(mothers:apps/scratchpad/examples/mothers).
