a3984687e7a0de27dec9697df59e002de55068ec
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-05-25 14:31:42 +0200

    FIXED: debug.pl should not load run.pl

--- debug.pl
+++ debug.pl
@@ -2,7 +2,7 @@
 % at http://localhost:3030/  It starts the graphical thread monitor to
 % make it easy to see that pengines are created and destroyed.
 
-:- load_files(run, [silent(true)]).
+:- load_files(load, [silent(true)]).
 
 :- initialization prolog_ide(thread_monitor).
 :- initialization server(3030).
