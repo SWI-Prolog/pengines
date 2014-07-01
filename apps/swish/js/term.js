6c91f2c2a13e77f4605923700280583f210cc183
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-07-01 16:27:39 +0200

    ADDED: Fold and unfold of Prolog terms.

---
+++ apps/swish/js/term.js
@@ -0,0 +1,14 @@
+if (!window.console) { window.console = {log: function(){} } }
+
+function unfold() {
+	console.log('unfold:' + $(this).text())
+	$(this).next().toggleClass('fold')
+	$(this).remove()
+}
+
+$(document).on('click', '.pl-functor, .pl-infix', function() {
+	var p = $(this).parent()
+	console.log('toggling struct ' + $(p).text())
+	$(p).toggleClass('fold')
+	$(p).before('<span class="pl-ellipsis">...</span>').prev().click(unfold)
+})
