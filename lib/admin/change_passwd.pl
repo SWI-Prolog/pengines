d163af2e7102f023aa567b7dae222992eceb5e7e
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-02-05 18:45:51 +0100

    Cleanup code to set the admin passwd

--- lib/admin/change_passwd.pl
+++ lib/admin/change_passwd.pl
@@ -1,9 +1,11 @@
-:- module(change_passwd, []).
-
+:- module(change_passwd,
+	  [ change_passwd/3
+	  ]).
 
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/http_parameters)).
+:- use_module(library(http/http_authenticate)).
 :- use_module(library(crypt)).
 
 
@@ -15,14 +17,24 @@ change_passwd(Request) :-
             [ passwd(Passwd, [])
             ]),
     change_passwd(passwd, admin, Passwd),
-    reply_json(json([ok= @true])).
+    reply_json(json{ok:true}).
 
 
 change_passwd(File, User, Passwd) :-
+    phrase("$1$", EncryptedPasswd, _),
     crypt(Passwd, EncryptedPasswd),
-    setup_call_cleanup(
-	open(File, write, Stream,
-	     [ lock(write)
-	     ]),
-	format(Stream, '~p:~s\n', [User, EncryptedPasswd]),
-	close(Stream)).
+    atom_codes(Hash, EncryptedPasswd),
+    with_mutex(pengine_passwd,
+	       update_passwd(File, User, Hash)).
+
+update_passwd(File, User, Hash) :-
+    exists_file(File), !,
+    http_read_passwd_file(File, Data),
+    (	select(passwd(User, _, Fields), Data,
+	       passwd(User, Hash, Fields), NewData)
+    ->	true
+    ;   append(Data, [passwd(User, Hash, [])], NewData)
+    ),
+    http_write_passwd_file(File, NewData).
+update_passwd(File, User, Hash) :-
+    http_write_passwd_file(File, passwd(User, Hash, [])).
